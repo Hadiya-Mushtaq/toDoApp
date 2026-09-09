bf0b7e10c5dfac629e838ec5d15e3f3e65f86712
Hadiya Mushtaq <hadiyamushtaqnowshahri19@gmail.com>
2026-09-09 15:44:47 +0530

    fix: remove default refresh behaviour + added taskList state + empty input field on add

--- src/component/ToDoInput.jsx
+++ src/component/ToDoInput.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 export default function ToDoInput() {
   const [inputValue, setInputValue] = useState('');
+  const [toDoList, setToDoList] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setToDoList([...toDoList, inputValue]);
+    setInputValue('');
+  }
   return (
     <div>
         <form>
             <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit">Add</button>
+            <button type="submit" onClick={(e)=>{handleSubmit(e)}}>Add</button>
         </form>
     </div>
   );
+   
 }
